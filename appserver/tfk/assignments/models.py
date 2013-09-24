618469659e7f030b7871d7db45ca45926f3361f8
sneha <singh1688@gmail.com>
2013-09-23 18:44:21 -0700

    Adding python and Django framework setup

---
+++ appserver/tfk/assignments/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Assignment(models.Model):
+	start_date = models.DateTimeField('start date')
+	end_date = models.DateTimeField('end date')
+	question = models.CharField(max_length=500)
+	result = models.CharField(max_length=100)
