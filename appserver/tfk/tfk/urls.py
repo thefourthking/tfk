ef2ffbc4bde23cbc30749da0e7ccf10a0f537ba0
Nikhil Mungel <hyfather@gmail.com>
2013-09-25 15:34:45 -0700

    Redirecting root url to the assignments app

--- appserver/tfk/tfk/urls.py
+++ appserver/tfk/tfk/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.http import HttpResponseRedirect
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -15,4 +16,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^assignments/', include('assignments.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', lambda r : HttpResponseRedirect('assignments/')),
 )
